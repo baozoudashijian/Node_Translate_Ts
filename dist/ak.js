e31e444c7668a88587d9fccf1f57d5c10d67fa3d
Roger Zhang <1787009521@qq.com>
2022-04-30 19:52:32 +0800

    feat: 删除dist

--- dist/ak.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.serect = exports.appid = void 0;
-exports.appid = '20220107001049335';
-exports.serect = 'Vh8jY5ezd5oUqakWOX9D';
--- dist/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.translate = void 0;
-const request_1 = require("./request");
-function translate(str) {
-    (0, request_1.requestToTranslate)(str);
-}
-exports.translate = translate;
