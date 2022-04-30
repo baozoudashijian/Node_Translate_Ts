fb2f813795573e7d70914c709714b3b999b6afdb
Roger Zhang <1787009521@qq.com>
2022-04-30 14:17:29 +0800

    feat: 实现中文与英文互相翻译

--- src/request.ts
+++ src/request.ts
@@ -5,17 +5,19 @@ import * as md5 from 'md5'
 import { appid, serect } from './ak'
 
 function requestToTranslate(q: string) {
-    
+
     const randomstr: string = randomstring.generate(10)
     const sign = generateSign(appid, q, randomstr, serect)
+    const { from, to } = judgeTransDirection(q)
 
     const query = {
-        from: 'en',
-        to: 'zh',
+        
         appid,
         salt: randomstr,
         sign,
         q,
+        from,
+        to
     }
     const options = {
         hostname: 'fanyi-api.baidu.com',
@@ -41,6 +43,19 @@ function requestToTranslate(q: string) {
 // 生成签名
 function generateSign(appid, q, randomstr, serect) {
     return md5(appid + q + randomstr + serect)
+}
+//
+function judgeTransDirection(str: string) {
+    let result = {
+        from: 'zh',
+        to: 'en'
+    }
+    let firstLetter = str.charCodeAt(0)
+    if((firstLetter >= 65 && firstLetter <= 90) || (firstLetter >= 97 && firstLetter <= 122 )){
+        result.from = 'en'
+        result.to = 'zh'
+    }
+    return result
 }
 
 export { requestToTranslate }
